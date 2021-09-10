5a4a6dbbdad52adbe7d0c7e1880aa64c573d38c3
Sargass <alexey.simochkin@gmail.com>
2021-09-10 11:11:22 +0300

    Refactoring ViewController

--- HW2_2_15/ViewController.swift
+++ HW2_2_15/ViewController.swift
@@ -7,42 +7,61 @@
 
 import UIKit
 
+enum CurrentColor {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var ViewRedUIView: UIView!
     @IBOutlet weak var ViewYellowUIView: UIView!
     @IBOutlet weak var ViewGreenUIView: UIView!
+    
     @IBOutlet weak var StartButton: UIButton!
+    
+    private var currentColor = CurrentColor.red
+    private let lightOn: CGFloat = 1.0
+    private let lightOff: CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ViewRedUIView.layer.cornerRadius = 67
-        ViewRedUIView.layer.opacity = 0.3
-        
-        ViewYellowUIView.layer.cornerRadius = 67
-        ViewYellowUIView.layer.opacity = 0.3
-        
-        ViewGreenUIView.layer.cornerRadius = 67
-        ViewGreenUIView.layer.opacity = 0.3
-        
         StartButton.layer.cornerRadius = 15
+        
+        ViewRedUIView.alpha = lightOff
+        ViewYellowUIView.alpha = lightOff
+        ViewGreenUIView.alpha = lightOff
+        
+    }
+    
+    override func viewWillLayoutSubviews() {
+        
+        ViewRedUIView.layer.cornerRadius = ViewRedUIView.frame.width / 2
+        ViewYellowUIView.layer.cornerRadius = ViewYellowUIView.frame.width / 2
+        ViewGreenUIView.layer.cornerRadius = ViewGreenUIView.frame.width / 2
         
     }
     
     @IBAction func StartButtonPressed() {
-        if ViewRedUIView.layer.opacity == 0.3 && ViewYellowUIView.layer.opacity == 0.3 && ViewGreenUIView.layer.opacity == 0.3 {
-            ViewRedUIView.layer.opacity = 1
-        } else if ViewRedUIView.layer.opacity == 1 {
-            ViewRedUIView.layer.opacity = 0.3
-            ViewYellowUIView.layer.opacity = 1
-        } else if ViewYellowUIView.layer.opacity == 1 {
-            ViewYellowUIView.layer.opacity = 0.3
-            ViewGreenUIView.layer.opacity = 1
-        } else if ViewGreenUIView.layer.opacity == 1 {
-            ViewRedUIView.layer.opacity = 0.3
-            ViewYellowUIView.layer.opacity = 0.3
-            ViewGreenUIView.layer.opacity = 0.3
+        
+        StartButton.setTitle("NEXT", for: .normal)
+        
+        switch currentColor {
+        case .red:
+            ViewGreenUIView.alpha = lightOff
+            ViewRedUIView.alpha = lightOn
+            currentColor = .yellow
+            
+        case .yellow:
+            ViewRedUIView.alpha = lightOff
+            ViewYellowUIView.alpha = lightOn
+            currentColor = .green
+            
+        case .green:
+            ViewYellowUIView.alpha = lightOff
+            ViewGreenUIView.alpha = lightOn
+            currentColor = .red
+            
         }
     }
 
